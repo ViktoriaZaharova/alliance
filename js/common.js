b150f3c271776cb925a85f1ff15b1a8f0568a423
Виктория Захарова <zaharova181114@gmail.com>
2023-05-31 10:21:33 +0300

    add 3 pages

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.btn-burger').on('click', function () {
+	$('.nav-menu').fadeToggle();
+});
+
+Fancybox.bind("[data-fancybox]");
+
+$('.article-gallery').slick({
+	slidesToshow: 4,
+	infinite: false,
+	variableWidth: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
