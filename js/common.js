6f9a148174ea76ff42de94b829962dbecc9ac45b
Виктория Захарова <zaharova181114@gmail.com>
2023-06-10 18:10:24 +0300

    add about

--- js/common.js
+++ js/common.js
@@ -63,8 +63,51 @@ $('.news-slider').slick({
 $('.posts-slider').slick({
 	slidesToShow: 1,
 	variableWidth: true,
-	arrows: false
-})
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 2000,
+});
+
+$('.history-slider').slick({
+	slidesToShow: 1,
+	arrows: false,
+	variableWidth: true,
+});
+
+$('.help-social-slider').slick({
+	slidesToShow: 1,
+	variableWidth: true,
+	arrows: false,
+	// autoplay: true,
+	// autoplaySpeed: 2000,
+});
+
+
+$('.diplomas-slider').slick({
+	slidesToShow: 5,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 4,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 2,
+			}
+		}
+	]
+});
 
 $('.article-gallery').slick({
 	slidesToShow: 4,
@@ -247,4 +290,15 @@ $('.btn-filter').click(function () {
 
 $('.select-property-wrapper__close').click(function () {
 	$('.select-property-wrapper').fadeOut();
+});
+
+$('.go_to').click(function (e) {
+	e.preventDefault();
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length !== 0) {
+		$('html, body').animate({
+			scrollTop: $(scroll_el).offset().top
+		}, 500);
+	}
+	return false;
 });
